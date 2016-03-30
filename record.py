2a97740348d29949dde84b8de45c75afd58248df
kmein <kieran.meinhardt@gmail.com>
2016-03-30 07:08:12 +0200

    Wrap code in try.

--- record.py
+++ record.py
@@ -11,26 +11,26 @@ WAVE_OUTPUT_FILENAME = "output.wav"
 
 def main():
     audio = pyaudio.PyAudio()
+    try:
+        stream = audio.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            frames_per_buffer=CHUNK)
 
-    stream = audio.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=CHUNK)
+        print("* recording")
 
-    print("* recording")
+        frames = []
+        for _ in range(0, int(RATE/CHUNK * RECORD_SECONDS)):
+            data = stream.read(CHUNK)
+            frames.append(data)
 
-    frames = []
-    for i in range(0, int(RATE/CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    print("* done recording")
-
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
+        print("* done recording")
+    finally:
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
 
     wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
     wf.setnchannels(CHANNELS)
